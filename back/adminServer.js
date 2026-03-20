a9c6eb0de633c41748062f10d04d6ca89a95f670
Cyanide <miqueas736d@gmail.com>
2026-03-20 14:42:23 -0300

    feat: fixing static files route

--- back/adminServer.js
+++ back/adminServer.js
@@ -1,25 +1,47 @@
-const express = require("express")
-
-const pug = require("pug")
-
-const app = express()
-
-const path = require("path")
-
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine","pug")
 
 
-app.use(express.json())
+const express = require("express");
 
-const Route = require("./routers/router")
-app.use("/",Route)
+const app = express();
 
-// app.get("/", (req,res) => {
-//     res.render('index',{title:"hey",user:"miki"})
-// })
+require("./middlewares/db");
 
-app.listen(5000, ()=>{
 
-    console.log("Server running http://localhost:5000");
+
+const path = require("path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+const methodOverride = require("method-override");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      const method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  }),
+);
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+app.use("/static", express.static(path.join(__dirname, "static")));
+
+const Route = require("./routers/router");
+
+app.use("/", Route);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+
+app.listen(5000, () => {
+  console.log("Server running http://localhost:5000");
 });
