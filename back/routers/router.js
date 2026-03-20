02b884345f1585909a0dd14ef0c9aee02fddfa7e
Cyanide <miqueas736d@gmail.com>
2026-03-20 14:44:26 -0300

    remove deleted files

--- back/routers/router.js
+++ back/routers/router.js
@@ -1,8 +1,23 @@
 const express = require("express")
 const router = express.Router()
 
-const controller = require("../controllers/controller")
+const auth = require("../middlewares/auth")
+const logincontroller = require("../controllers/loginController")
+const dashboardcontroller = require("../controllers/dashboardController")
 
 
 
+
+router.post("/login",logincontroller.login)
+router.delete("/logout",dashboardcontroller.logout)
+router.get("/dashboard",auth.authenticate,dashboardcontroller.populate)
+
+router.post("/dashboard/addProduct",auth.authenticate,dashboardcontroller.addProduct)//add auth here with middleware
+router.delete("/dashboard/removeProduct",auth.authenticate,dashboardcontroller.removeProduct)//add auth here with middleware
+router.put("/dashboard/updateProduct",auth.authenticate,dashboardcontroller.updateProduct)//add auth here with middleware
+
+// crud de productos 
+
+
+module.exports = router
 
