9e05009ad49576cdf406c0abf97b6493ba6d86e5
Cyanide <miqueas736d@gmail.com>
2026-03-23 14:57:20 -0300

    fix: fixing last dependencies issues

---
+++ front/storeServer.js
@@ -0,0 +1,48 @@
+
+
+const express = require("express");
+
+const app = express();
+
+require("./middlewares/db");
+
+const path = require("path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+const methodOverride = require("method-override");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      const method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  }),
+);
+const session = require("express-session")
+
+app.use(session({
+  secret: "pengu-secret",
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use("/static", express.static(path.join(__dirname, "static")));
+
+const Route = require("./routers/router");
+
+app.use("/", Route);
+app.get("/", (req, res) => {
+  res.render("store");
+});
+
+
+app.listen(5001, () => {
+  console.log("Server running http://localhost:5001");
+});
